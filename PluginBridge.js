88ae3657641a0da2bf857842993fd665a92aa4e9
lianurwahdini <80952751+lianurwahdini@users.noreply.github.com>
2025-05-21 15:29:11 +0700

    Add files via upload

---
+++ PluginBridge.js
@@ -0,0 +1,31 @@
+let unityInstance;
+function SendPositionToUnity(x,y,z) {
+  if (unityInstance) {
+    unityInstance.SendMessage('WebARManager', 'OnReceivePosition', `${x},${y},${z}`);
+  }
+}
+async function startCameraAndTrack() {
+  const video = document.createElement('video');
+  video.setAttribute('autoplay', '');
+  video.setAttribute('playsinline', '');
+  document.body.appendChild(video);
+  const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+  video.srcObject = stream;
+  video.onloadedmetadata = ()=> {
+    video.play();
+    trackLoop();
+  };
+  window.unityInstance = unityInstance = unityInstance || window.unityInstance;
+}
+function trackLoop() {
+  // contoh sederhana: kirim titik (0,0,0) terus-menerus
+  SendPositionToUnity(0, 0, 0);
+  requestAnimationFrame(trackLoop);
+}
+
+function SendPositionToUnity(x, y, z) {
+  console.log("Kirim ke Unity:", x, y, z);
+  if (unityInstance) {
+    unityInstance.SendMessage('WebARManager', 'OnReceivePosition', `${x},${y},${z}`);
+  }
+}
